806d87a837dac30c0d12517b007084126e4dc1c9
Rayson Simon <raysonsimon56@gmail.com>
2026-01-02 18:47:47 +0300

    cors configuration for vite app

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const taskRoutes = require("./routes/task.routes");
 const authRoutes = require("./routes/auth.routes");
 const errorMiddleware = require("./middlewares/error.middleware");
@@ -6,6 +7,13 @@ const errorMiddleware = require("./middlewares/error.middleware");
 const app = express();
 
 app.use(express.json());
+// Allow CORS from your frontend
+app.use(
+    cors({
+        origin: "http://localhost:5173", // frontend origin
+        credentials: true, // if you send cookies (optional)
+    })
+);
 
 app.use("/api/v1/tasks", taskRoutes);
 app.use("/api/v1/auth", authRoutes);
